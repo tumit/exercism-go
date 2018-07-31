11fb3a88c05b7541ca7610bf728b5579783bf466
tumit <tumit@live.com>
2018-07-31 13:25:40 +0700

    add more comment

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -2,7 +2,12 @@ package raindrops
 
 import "strconv"
 
-// Convert return
+// Convert a number to a string, the contents of which depend on the number's factors.
+// - If the number has 3 as a factor, output 'Pling'.
+// - If the number has 5 as a factor, output 'Plang'.
+// - If the number has 7 as a factor, output 'Plong'.
+// - If the number does not have 3, 5, or 7 as a factor,
+//   just pass the number's digits straight through.
 func Convert(n int) (r string) {
 
 	if n%105 == 0 {
