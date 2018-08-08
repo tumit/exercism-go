7b9d09d790134d59360aff2ec8726bff82eb2193
tumit <tumit@live.com>
2018-08-08 23:08:46 +0700

    add gigasecond solution

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,18 +1,8 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package gigasecond
 
-// import path for the time package from the standard library
 import "time"
 
-// AddGigasecond should have a comment documenting it.
+// AddGigasecond its return add a Gigasecond.
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t
+	return t.Local().Add(time.Second * time.Duration(1000000000))
 }
