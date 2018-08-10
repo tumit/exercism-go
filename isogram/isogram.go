1b9825622881912a317927344d82f426d9318cd9
tumit <tumit@live.com>
2018-08-10 09:40:27 +0700

    add isogram solution: use map of rune

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,25 +1,25 @@
 package isogram
 
-import "strings"
-
-var ignores = "- "
-
-func removeIgnoreString(s string) string {
-	for _, ignore := range ignores {
-		s = strings.Replace(s, string(ignore), "", -1)
-	}
-	return s
-}
+import (
+	"unicode"
+)
 
 // IsIsogram it return is isogram
 func IsIsogram(s string) bool {
 
-	s = strings.ToUpper(removeIgnoreString(s))
+	foundMap := make(map[rune]bool)
 
 	for _, r := range s {
-		if strings.Count(s, string(r)) != 1 {
+
+		if !unicode.IsLetter(r) {
+			continue
+		}
+
+		if _, found := foundMap[unicode.ToUpper(r)]; found {
 			return false
 		}
+
+		foundMap[unicode.ToUpper(r)] = true
 	}
 
 	return true
