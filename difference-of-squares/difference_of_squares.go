f18648e88b38d639991caac2e26f837816bdfe42
tumit <tumit@live.com>
2018-08-14 10:47:35 +0700

    add difference-of-squares solution

---
+++ difference-of-squares/difference_of_squares.go
@@ -0,0 +1,22 @@
+package diffsquares
+
+import (
+	"math"
+)
+
+// SquareOfSums it return square of sums
+func SquareOfSums(n int) int {
+	sum := (n * (1 + n)) / 2
+	return int(math.Pow(float64(sum), float64(2)))
+}
+
+// SumOfSquares it return sum of squares
+func SumOfSquares(n int) int {
+	sum := (n * (n + 1) * ((2 * n) + 1)) / 6
+	return int(sum)
+}
+
+// Difference it return difference of square of sums and sum of square
+func Difference(n int) int {
+	return SquareOfSums(n) - SumOfSquares(n)
+}
