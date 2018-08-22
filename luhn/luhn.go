64b9186b6e48f8d242298bbb34daf06f212f09c5
tumit <tumit@live.com>
2018-08-22 09:59:40 +0700

    add luhn solution

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,5 +1,54 @@
 package luhn
 
+import (
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+func rToi(r rune) int {
+	buf := make([]byte, 1)
+	_ = utf8.EncodeRune(buf, r)
+	tmp, _ := strconv.Atoi(string(buf))
+	return tmp
+}
+
+// Valid it return valid status of data
 func Valid(s string) bool {
-	return false
+
+	s = strings.Replace(s, " ", "", -1)
+
+	if len(s) <= 1 {
+		return false
+	}
+
+	sum := 0
+
+	rs := []rune(s)
+
+	for i := 0; i < len(rs); i++ {
+
+		r := rs[len(rs)-1-i]
+		if unicode.IsNumber(r) {
+			v := rToi(r)
+			if i%2 == 1 {
+				v *= 2
+				if v > 9 {
+					v -= 9
+				}
+			}
+			sum += v
+		} else {
+			sum = -1
+			break
+		}
+	}
+
+	if sum == -1 {
+		return false
+	}
+
+	return sum%10 == 0
+
 }
